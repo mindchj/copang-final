2e38ad607aeb2d613f8352e39d80ac18d9a1cda6
Your Name <you@example.com>
2021-07-04 12:47:27 +0900

    productRedux 적용 위해 RouteMain, Menu 수정

--- src/RouteMain.js
+++ src/RouteMain.js
@@ -4,17 +4,22 @@ import Menu from "./Menu";
 
 import Cart from "./purchase/Cart";
 import AddNewProductApp from "./product/AddNewProduct/AddNewProductApp";
-import OrderComplete from "./purchase/OrderComplete";
+import {Provider} from 'react-redux';
+import Container, {store} from './product/AddNewProduct/productRedux';
 import ProductListRouteMain from './youngjae/ProductListRouteMain';
 import MyCopang from "./member/MyCopang";
 import RouteHyunjin from "./hyunjin/RouteHyunjin";
+import OrderComplete from "./purchase/OrderComplete";
 
 import TopBar from "./TopBar/TopBar";
 import LoginPage from "./TopBar/Component/LoginPage";
 import RegisterPage from "./TopBar/Component/RegisterPage";
 import OrderPageApp from "./purchase/OrderPageApp";
 
+// login redux와 product redux 중복 문제 해결할 것..
+
 const RouteMain = () => {
+
   return (
     <div>
       <BrowserRouter>
@@ -22,7 +27,11 @@ const RouteMain = () => {
         <Menu />
         <Route path="/member/1" component={Cart} />
         <Route path="/member/2" component={AddNewProductApp} />
-        <Route path="/member/3" component={OrderComplete}/>
+        <Route path="/member/3">
+          <Provider store={store}>
+            <Container/>
+          </Provider>
+        </Route>
         <Route path="/member/4" component={ProductListRouteMain} />
         <Route path="/member/5" component={MyCopang} />
         <Route path="/member/6" component={RouteHyunjin} />
--- src/product/AddNewProduct/productRedux.js
+++ src/product/AddNewProduct/productRedux.js
@@ -1,24 +1,30 @@
-import { connect } from "formik";
+import { connect } from "react-redux";
+import { createStore } from "redux";
 import { createAction, handleActions } from "redux-actions";
+import Example from './Example';
 
+// 1. 액션타입 정의
 const SET_ITEM_NAME = "setItemName";
-const SET_ITEM_CONTENT = "setItemContent";
+const SET_ITEM_COMMENT = "setItemComment";
 const ADD_FORM_LIST = "addFormList";
 const DEL_FORM_LIST = "delFormList";
 const MOD_FORM_LIST = "modFormList";
 
+// 2. 액션타입 생성 (redux-actions의 createAction 함수 사용)
 const setItemName = createAction(SET_ITEM_NAME, itemName => itemName);
-const setItemContent = createAction(SET_ITEM_CONTENT, itemContent => itemContent);
+const setItemComment = createAction(SET_ITEM_COMMENT, itemComment => itemComment);
 const addFormList = createAction(ADD_FORM_LIST, form => form);
 const delFormList = createAction(DEL_FORM_LIST, idx => idx);
 const modFormList = createAction(MOD_FORM_LIST, (form, idx) => ({ form: form, idx: idx }));
 
+// 3. store state 초기값
 const init = {
     itemName : "",
     itemComment : "",
     itemDetailFormList : [],
 }
 
+// 4. 액션처리 리듀서 - 받아온 액션타입에 맞게 store state를 변경
 export const setter = handleActions(
     {
         [SET_ITEM_NAME] : (state, action) => (
@@ -27,7 +33,7 @@ export const setter = handleActions(
                 itemName : action.payload,
             }
         ),
-        [SET_ITEM_CONTENT] : (state, action) => (
+        [SET_ITEM_COMMENT] : (state, action) => (
             {
                 ...state,
                 itemComment : action.payload,
@@ -54,25 +60,47 @@ export const setter = handleActions(
     init,
 )
 
-const Container = ({product, setItemName, setItemContent, addFormList, delFormList, modFormList}) => {
+// 5. store 생성후 액션처리 리듀서와 연결 -> <Provider store={store}> 형식으로 import
+export const store = createStore(setter);
+
+// 6. Provider 내부에서 store와 통신할 Container 컴포넌트 정의
+const Container = ({product, setItemName, setItemComment, addFormList, delFormList, modFormList}) => {
 
     return (
         <div>
-            <Example/>
+            <Example product={product} setItemName={setItemName} setItemComment={setItemComment}
+                     addFormList={addFormList} delFormList={delFormList} modFormList={modFormList} />
         </div>
     )
 
 }
 
+// 7. Container 컴포넌트에 store state 와 액션처리 리듀서 매핑
 export default connect(
     state => ({
         product : state,
     }),
     {
         setItemName,
-        setItemContent,
+        setItemComment,
         addFormList,
         delFormList,
         modFormList,
     }
 )(Container);
+
+
+// 정리
+// 연결관계 : Provider는 store state & 액션처리 리듀서와 연결되어 있음
+// 연결관계2 : Container는 store state & 액션타입 생성함수와 연결되어 있음
+// 리덕스 흐름설명 예시)
+
+// Container에서 setItemName("abc") 액션타입 생성함수를 실행하면, 
+// Provideer의 액션처리 리듀서의 [SET_ITEM_NAME] 필드로 이동하여 "abc"값을 넘겨줌
+// 액션처리 리듀서는 store state의 itemName 필드값을 "abc"로 변경
+// Container는 store state의 변경된 값을 props로 받아서 리렌더링
+
+// 즉 Container 액션생성 -> Provider 액션처리 -> Container 리렌더링 순서
+// 따라서 Container는 store state와 액션생성함수를 매핑시켜야 하고,
+// Provider는 store state와 액션처리 함수를 매핑시켜야 한다.
+
