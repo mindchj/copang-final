9e17aee704cfda369093988b06bc1bb5046a2c1f
chj <830731x@naver.com>
2021-07-06 14:18:11 +0900

    테스트코드 업데이트

--- src/hyunjin/Test22.js
+++ src/hyunjin/Test22.js
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { MailRounded, Restaurant } from "@material-ui/icons";
+import { useEffect, useState } from "react";
 
 const Test22 = () => {
     const [imgList, setImglist] = useState([]);
     const [itemList, setItemList] = useState([]);
     const [item, setItem] = useState({});
     const [idx, setIdx] = useState();
+    const [test,setTest] = useState();
+    const [refresh, setRefresh] = useState(0);
 
     const inputChange = (e) => {
         setItem({
@@ -15,7 +18,9 @@ const Test22 = () => {
     const addItem = () => {
         setItemList([
             ...itemList,
-            item,
+            {
+                ...item,
+            }
         ])
     }
     const deleteItem = (idx) => {
@@ -23,15 +28,26 @@ const Test22 = () => {
             itemList.filter( (row,rowidx) => rowidx != idx )
         )
     }
-    const addItemImg = (e, idx) => {
-        const newList = itemList;
-        newList[idx].img = e.target.files[0];
-        setItemList(newList);
+    const addItemImg = (file, idx) => {
+        
+        itemList[idx].img = URL.createObjectURL(file);
+
+        setRefresh(prev => prev+1);
     }
+
+    useEffect( () => {
+        console.log(itemList);
+    },[refresh])
     
     
     return (
         <div>
+            <select>
+                <option>1</option>
+            </select>
+            {JSON.stringify(itemList)}
+            <br/><input type="file" name="test" onChange={(e)=>setTest(URL.createObjectURL(e.target.files[0]))}/><br/>
+            {test && <img src={test}/>}<br/>
             idx : <input name="idx" onChange={(e)=>setIdx(e.target.value)}/><br/>
             name : <input name="name" onChange={inputChange}/><br/>
             desc : <input name="desc" onChange={inputChange}/><br/>
@@ -46,9 +62,9 @@ const Test22 = () => {
                         <td>{idx}</td>
                         <td>{row.name}</td>
                         <td>{row.desc}</td>
-                        <td><img src={URL.createObjectURL(row.img)}/></td>
+                        <td>{row.img && <img src={row.img}/>}</td>
                         <td>
-                            <input type="file" name="img" onChange={(e)=>addItemImg(e, idx)}/>
+                            <input type="file" name="img" onChange={(e)=>addItemImg(e.target.files[0], idx)}/>
                         </td>
                     </tr>
                 )}
